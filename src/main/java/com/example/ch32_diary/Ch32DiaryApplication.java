f32d6e3d6d010121a517bddfb3197b43433dbd0b
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-16 20:02:59 +0900

    과제 시작

---
+++ src/main/java/com/example/ch32_diary/Ch32DiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch32_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch32DiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch32DiaryApplication.class, args);
+    }
+
+}
