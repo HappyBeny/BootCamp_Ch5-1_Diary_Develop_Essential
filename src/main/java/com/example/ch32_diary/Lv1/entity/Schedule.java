75d63183b8c0cf6d781bd144dc59fb5e7bcbb21f
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-17 16:44:39 +0900

    수정을 위한 세터 추가

--- src/main/java/com/example/ch32_diary/Lv1/entity/Schedule.java
+++ src/main/java/com/example/ch32_diary/Lv1/entity/Schedule.java
@@ -2,6 +2,7 @@ package com.example.ch32_diary.Lv1.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "schedule")
