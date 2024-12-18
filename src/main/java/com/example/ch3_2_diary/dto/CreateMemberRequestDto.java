049a14cb68e6970f1cadeb7794e48ce7b83d1614
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-18 17:06:17 +0900

    Dto에 패스워드 추가

--- src/main/java/com/example/ch3_2_diary/dto/CreateMemberRequestDto.java
+++ src/main/java/com/example/ch3_2_diary/dto/CreateMemberRequestDto.java
@@ -1,15 +1,13 @@
 package com.example.ch3_2_diary.dto;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CreateMemberRequestDto {
 
     private final String username;
+    private final String password;
     private final String email;
-
-    public CreateMemberRequestDto(String username, String email) {
-        this.username = username;
-        this.email = email;
-    }
 }
