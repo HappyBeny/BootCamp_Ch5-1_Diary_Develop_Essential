fcec3d802b60d4e7fe822536655d6058a994fa6f
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-18 17:06:07 +0900

    멤버삭제용 Dto 생성

---
+++ src/main/java/com/example/ch3_2_diary/dto/DeleteMemberRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.ch3_2_diary.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class DeleteMemberRequestDto {
+
+    private final Long id;
+    private final String password;
+}
