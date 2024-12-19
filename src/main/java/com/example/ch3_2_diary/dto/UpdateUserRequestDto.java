48771b3091439da924d0a6682b43f9421a93ab84
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-19 13:22:57 +0900

    비밀번호 바꿀 수 있게 수정

--- src/main/java/com/example/ch3_2_diary/dto/UpdateUserRequestDto.java
+++ src/main/java/com/example/ch3_2_diary/dto/UpdateUserRequestDto.java
@@ -1,12 +1,14 @@
 package com.example.ch3_2_diary.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UpdateUserRequestDto {
 
     private final String username;
-    private final String password;
+    private final String oldPassword;
+    private String newPassword;
 }
