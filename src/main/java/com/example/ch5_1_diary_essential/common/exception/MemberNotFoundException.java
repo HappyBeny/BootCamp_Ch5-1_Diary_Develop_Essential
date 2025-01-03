7a6d5cd83a3b6865e86326f923d4339a4bd082c6
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-01-03 16:55:01 +0900

    📝 update : changed from DeletedUserException

---
+++ src/main/java/com/example/ch5_1_diary_essential/common/exception/MemberNotFoundException.java
@@ -0,0 +1,5 @@
+package com.example.ch5_1_diary_essential.common.exception;
+
+public class MemberNotFoundException extends RuntimeException{
+    public MemberNotFoundException(String message) { super(message); }
+}
