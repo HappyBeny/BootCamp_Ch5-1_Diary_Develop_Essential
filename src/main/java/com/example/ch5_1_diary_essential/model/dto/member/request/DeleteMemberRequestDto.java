9b117e0e31efa3198306b6edcea8afc19128aafd
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-01-03 11:17:42 +0900

    🧹 chore : removed 'final' of fields, changed @RAC to @AAC

--- src/main/java/com/example/ch5_1_diary_essential/model/dto/member/request/DeleteMemberRequestDto.java
+++ src/main/java/com/example/ch5_1_diary_essential/model/dto/member/request/DeleteMemberRequestDto.java
@@ -1,13 +1,13 @@
 package com.example.ch5_1_diary_essential.model.dto.member.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class DeleteMemberRequestDto {
 
     @NotBlank(message = "비밀번호를 입력해주세요")
-    private final String password;
+    private String password;
 }
--- src/main/java/com/example/ch5_1_diary_essential/model/dto/member/request/LoginRequestDto.java
+++ src/main/java/com/example/ch5_1_diary_essential/model/dto/member/request/LoginRequestDto.java
@@ -2,17 +2,17 @@ package com.example.ch5_1_diary_essential.model.dto.member.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDto {
 
     @NotBlank(message = "이메일 주소를 입력해주세요")
     @Email(message = "이메일 주소는 올바른 형식이어야 합니다.")
-    private final String email;
+    private String email;
 
     @NotBlank(message = "비밀번호를 입력해주세요")
-    private final String password;
+    private String password;
 }
